f5668c7c28d7a569318b44e932ca0caaddd3d94a
Marcel Fahle <m.fahle@gmail.com>
2012-07-28 05:53:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mochastudy::Application.config.secret_token = '2097a3ae42d24e74e66bc378b9cc0fe1aeff16fb532b3c57d675807c7bf63015b5e71b7f1a9ecc36ff6dc7c79bedecd4438b75aae8b6c03a82d7b447096ac215'
